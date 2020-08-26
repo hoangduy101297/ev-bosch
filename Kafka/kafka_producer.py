0a40038cef526db2c7cf3a9843d9f049e3534206
ducbinh1996 <ducbinh1996@gmail.com>
2020-08-26 23:49:04 +0700

    Commit to testing on Pi

--- Kafka/kafka_producer.py
+++ Kafka/kafka_producer.py
@@ -1,10 +1,38 @@
 import time
-
+import json
 from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=['xvc-bosch.westus.cloudapp.azure.com:9092'])
 
+producer = KafkaProducer(bootstrap_servers=['xvc-bosch.westus.cloudapp.azure.com:9092'])
+current_milli_time = int(round(time.time() * 1000))
+send_data = {
+    "action": "TEST_DATA",
+    "timestamp": "%d" %(current_milli_time),
+    "macId": "d037456fab5d",
+    "timeZone": "GMT+7:00",
+    "dataType": "PI",
+    "data": {
+      "point": [
+      {
+        "type": "PI",
+        "speed_limit": 100, 
+        "battery_status": 10, 
+        "front_wh_speed": 22, 
+        "rear_wh_speed": 20, 
+        "odo_meter": 10, 
+        "battery_voltage": 10, 
+        "battery_current": 9,
+        "error_message": "error1",
+        "front_break": 1, 
+        "rear_break": 0, 
+        "outrigger_detection": 1
+      }
+    ]
+  }
+}
+print(json.dumps(send_data, indent=2))
 try:
-    producer.send('vsk-topic', value='testtttttttttt')
-    time.sleep(0.1)
+    producer.send('vsk-topic', value=send_data)
+    time.sleep(5)
+    print("success")
 except Exception as ex:
     print(ex)
