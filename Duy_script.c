bcdb4c7104dc5c39be5715d2e3a96f83220f6dd6
Nguyen Tran Hoang Duy (RBVH/EJV61) <ydu2hc@bosch.com>
2020-11-14 15:27:56 +0700

    writing TechTalk Script at home huhu

---
+++ Duy_script.c
@@ -0,0 +1,32 @@
+/* Script written by Duy - Do not copy =))) */
+
+1/ 'Thanks and continue the presentation:'
+
+2/  'Demand/Requirement:'
+	- Connectivity
+
+3/ 'Problem:'
+	- Input & output of Gatewate
+	- COM directions
+
+-> 4/ 'Solution:'
+	- Tech used: Communication Protocol [Kafka/ MQTT]
+	- Data Structure [how to be flexible]
+	- Threading [how to proceed multiple data stream]
+
+5/ 'Gateway SW design:'
+	- From Main thread coming to others
+
+6/ 'Solved and Unsolved issue:'
+    - Solved:
+		+ Different formats & protocol between VCU, IVT, MobileApp & VSK
+		+ Threading
+
+	- Unsolved:
+		+ Delay time [mention: MQTT is faster than Kafka]
+		+ Software is not robust: behavior when CAN timeout, MQTT/ Kafka timeout, ...
+
+/*------------------------------------------------------------------*/
+1/
+	Thanks a. Tùng for a super interesting session with the VCU software.
+	Alright guys, 
